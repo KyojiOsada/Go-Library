505d6f1df1d9e99e9fbd2cf781fd5fd668b0fcf0
Kyoji Osada <39667755+KyojiOsada@users.noreply.github.com>
2018-12-24 08:15:08 +0900

    Create udp6_multicast_sender.go

---
+++ udp6_multicast_sender.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+func main() {
+	ipv6 := "[ff02::1%eth0]"
+	port := ":9999"
+	addr := ipv6 + port
+	wait_time := 1
+	format := "Message "
+
+	fmt.Println("Sender:", addr)
+	conn, err := net.Dial("udp", addr)
+	_error(err)
+	defer conn.Close()
+
+	c := 0
+	for {
+		time.Sleep(time.Duration(wait_time) * time.Second)
+		message := format + strconv.Itoa(c)
+		conn.Write([]byte(message))
+		fmt.Printf("%s\n", message)
+		c++
+	}
+}
+
+func _error(_err error) {
+	if _err != nil {
+		panic(_err)
+	}
+}
